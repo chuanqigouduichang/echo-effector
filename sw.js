b6cf1ecdc391cd7444d53a18b515c2460f66d7a7
chuanqigouduichang <chuanqigouduichang@qq.com>
2026-06-04 00:01:02 +0800

    Initial: PWA echo effector with Web Audio API

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'echo-v1';
+const ASSETS = ['./', 'index.html', 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      if (resp && resp.status === 200) {
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return resp;
+    }).catch(() => caches.match(e.request)))
+  );
+});
